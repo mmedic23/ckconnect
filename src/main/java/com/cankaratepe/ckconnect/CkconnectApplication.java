823e829788bc5b7ff144bf39f6e60575ae217c49
mmedic23 <>
2025-02-13 22:30:23 +0300

    Initial commit

---
+++ src/main/java/com/cankaratepe/ckconnect/CkconnectApplication.java
@@ -0,0 +1,13 @@
+package com.cankaratepe.ckconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cankaratepe/ckconnect/CkconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cankaratepe.ckconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
