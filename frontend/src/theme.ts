3a72645a6a1ac18083d81705176c143c5205d4c8
mmedic23 <>
2025-02-20 20:59:44 +0300

    Add padding to input labels

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,5 +1,14 @@
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineThemeOverride, TextInput } from '@mantine/core';
 
 export const theme = createTheme({
   primaryColor: 'orange',
+  components: {
+    TextInput: {
+      styles: (theme: MantineThemeOverride) => ({
+        label: {
+          marginBottom: '0.25rem', // Use 'md' or 'xl' from Mantine's spacing scale
+        },
+      }),
+    },
+  },
 });
